24d19130efeeb4a735287256915f61b64ead5d53
wuzhanqiang <wuzhanqiang@wxchina.com>
2018-01-09 18:03:37 +0800

    scroll

--- app/src/main/java/net/yuanjin/mytest/viewstudy/ScrollerActivity.java
+++ app/src/main/java/net/yuanjin/mytest/viewstudy/ScrollerActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Scroller;
 
@@ -11,13 +12,18 @@ import net.yuanjin.R;
 import net.yuanjin.ui.BasicActivity;
 
 /**
- *  手势检测，用以辅助检测用户的单击、滑动、长按、双击等行为
+ *   Android Scroller完全解析，关于Scroller你所需知道的一切
+ *   http://blog.csdn.net/guolin_blog/article/details/48719871
  *  Created by zhan on 2018/01/04.
  */
 
 public class ScrollerActivity extends BasicActivity {
 
-    private MScrollView touchArea;
+    private LinearLayout layout;
+
+    private Button scrollToBtn;
+
+    private Button scrollByBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +33,43 @@ public class ScrollerActivity extends BasicActivity {
 
         getDefaultNavigation().setTitle("ScrollerActivity");
 
-        touchArea = (MScrollView) findViewById(R.id.testarea);
 
-        init();
+        layout = (LinearLayout) findViewById(R.id.layout);
+        scrollToBtn = (Button) findViewById(R.id.scroll_to_btn);
+        scrollByBtn = (Button) findViewById(R.id.scroll_by_btn);
+        scrollToBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.scrollTo(-60, -100);
+            }
+        });
+        scrollByBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.scrollBy(-60, -100);
+            }
+        });
 
     }
 
-    private void init() {
+    /**
+     * scrollTo(int x, int y)
+     * 让View相对于初始的位置滚动某段距离（View的初始位置是不变的）
+     * @param x 表示相对于当前位置横向移动的距离，正值向左移动，负值向右移动，单位是像素
+     * @param y 表示相对于当前位置纵向移动的距离，正值向上移动，负值向下移动，单位是像素
+     */
+    private void scrollTo(int x, int y){
 
-        touchArea.setClickable(true);
-        touchArea.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                touchArea.smoothScrollTo(100,-100);
-            }
-        });
+    }
+
+    /**
+     * scrollBy(int x, int y)
+     * 让View相对于当前的位置滚动某段距离
+     * @param x 表示相对于当前位置横向移动的距离，正值向左移动，负值向右移动，单位是像素
+     * @param y 表示相对于当前位置纵向移动的距离，正值向上移动，负值向下移动，单位是像素
+     */
+    private void scrollBy(int x, int y){
+
     }
 
 
